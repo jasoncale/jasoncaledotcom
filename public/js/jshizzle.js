4c7364872e2a86bab94616bce1add974774e0801
Jason Cale <jasoncale@jason-cales-macbook-pro-2.local>
2009-07-16 14:46:04 +0100

    testing tunes

--- public/js/jshizzle.js
+++ public/js/jshizzle.js
@@ -1,1 +1,8 @@
-ChiliBook.recipeFolder = "/js/";  
+$(document).ready(function() {
+  AudioPlayer.setup("http://jasoncale.com/player.swf", { width: 290 });
+
+  $('input.music').each(function (i) {
+    AudioPlayer.embed(("audioplayer_" + i), {soundFile: "http://jasoncale.com/tunes/"+$(this).attr('value')}); 
+  });
+
+});
