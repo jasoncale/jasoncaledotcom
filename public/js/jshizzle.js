35ad89cb66f954e6ef7e65dc2f6d68fabc7f1623
Jason Cale <jasoncale@jason-cales-macbook-pro-2.local>
2009-07-16 15:05:31 +0100

    added s3 for audio

--- public/js/jshizzle.js
+++ public/js/jshizzle.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   AudioPlayer.setup("http://jasoncale.com/player.swf", { width: 290 });
 
   $('input.music').each(function (i) {
-    AudioPlayer.embed(("audioplayer_" + i), {soundFile: "http://jasoncale.com/tunes/"+$(this).attr('value')}); 
+    AudioPlayer.embed(("audioplayer_" + i), {soundFile: "http://jasoncaleassets.s3-external-3.amazonaws.com/"+$(this).attr('value')}); 
   });
 
 });
