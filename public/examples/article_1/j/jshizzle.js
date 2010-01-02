66a2224f9235242491a4bf06ee2603750d79fd8c
Jason Cale <jasoncale@jase.config>
2010-01-02 16:33:26 +0000

    updated tutorial elements

--- public/examples/article_1/j/jshizzle.js
+++ public/examples/article_1/j/jshizzle.js
@@ -19,20 +19,50 @@
 
     },
     
-    enableLabelToggle: function() {
+    enableLabelToggle: function(action_callback) {
       $(this).createLink("", "action").click(function () {
-        //find the input related to label
-        var input = $(this).siblings('input:radio, input:checkbox');
+        // find the input related to label
+        // and define a function to retrieve it for later ..
+        $.fn.extend({
+          input: function () {
+              return $(this).siblings('input:radio, input:checkbox');
+            }
+        })
         
+        // create a local reference to it to use now ..
+        var input = $(this).input();
+                
         // is the input checked?
         var selected = !input.attr('checked');
-
-        // toggle the link's class name depending on input state
-        $(this).toggleClass('selected', selected);
-                
-        // toggle the input box
-        input.attr('checked', selected);
         
+        // we can add some behavioural rules here...
+        var canChange = true;
+        
+        // make sure we don't uncheck any radio buttons that are selected
+        if (input.attr('type') == "radio" && input.attr('checked')) canChange = false;
+        
+        if (canChange) {
+          // toggle the link's class name depending on input state
+          $(this).toggleClass('selected', selected);
+                
+          // toggle the input box
+          input.attr('checked', selected);
+        
+          // if the input element is a radio button
+          // we need to tell the other radio elements
+          // within the group that they have changed
+          // so they can update their associated links        
+          if (input.attr('type') == "radio") {
+            // find any inputs that have the same name,
+            // as they will be part of the same group
+            // and call change() as defined in the "each"
+            // function below
+            $('input[name="'+input.attr('name')+'"]').change();
+          };
+        };
+        
+        action_callback.apply($(this));
+
         return false;
       }).each(function () {
         
@@ -47,11 +77,14 @@
         var link = $(this);
                 
         // find inputs to bind change event on, then hide them
-        $(this).siblings('input:radio, input:checkbox').hide().change(function (){
-          
+        $(this).siblings('input:radio, input:checkbox').hide().change(function (){          
           // toggle the link's class name depending on input state
           link.toggleClass('selected', $(this).attr('checked'));
-        });
+        }).change();
+        
+        // I have appended a change() on the end of the change() definition
+        // to catch any initial states set on the form ..
+        
       });
       
       return $(this);
@@ -63,9 +96,23 @@
       
 (function ($) {
   
+  var employeeManagerCallback = function () {
+    console.log("FIRE AJAX FOR MANAGER!");
+  }
+  
+  var employeeAssistantCallback = function () {
+    console.log("FIRE AJAX FOR ASSISTANT!");
+  }
+  
+  var employeeRemoveCallback = function () {
+    $(this).parents().filter('.employee').fadeOut();
+  }
+  
   $.fn.extend({
     employeeWidget: function () {
-    
+      
+      $(this).addClass('js');
+      
       $(this).find('.employee').createLink("options", "toggle_options", true).click(function () {
         
         var link = $(this);
@@ -78,8 +125,12 @@
         
       }).siblings('.employee_options').hide();
     
-      $(this).find('.employee_options li label').enableLabelToggle();
+      $(this).find('.employee_options li.manager label').enableLabelToggle(employeeManagerCallback);
+      $(this).find('.employee_options li.assistant label').enableLabelToggle(employeeAssistantCallback);
+      $(this).find('.employee_options li.remove label').enableLabelToggle(employeeRemoveCallback);
+  
     }
+  
   });
   
 })(jQuery);
