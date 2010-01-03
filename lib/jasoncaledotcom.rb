b4e879c144a49aa6f293e6dd30edb6c4f78c83ac
Jason Cale <jasoncale@jase.config>
2010-01-03 16:53:40 +0000

    getting article together

--- lib/jasoncaledotcom.rb
+++ lib/jasoncaledotcom.rb
@@ -36,81 +36,82 @@ module Jasoncaledotcom
     
     class << self
       attr_accessor :article_dir
+      
+      def count(reset = false)
+        article_files(reset).size
+      end
+
+      def all(reset = false, published = true)
+        # remove any falses ..
+        (article_files(reset).map { |filename| Article.open(filename) }).select {|article| article && (article.published == published) }.reverse
+      end
+
+      def open(filename)
+        article = false
+
+        filename << ".yml" if !(filename =~ /.yml/)
+
+        if File.exist?(filename)
+          data = YAML.load_file(filename)
+          permalink = to_permalink(remove_ext(File.basename(filename)))
+
+          article = Article.new(data[:content], data, permalink)
+        end
+
+        article
+      end
+
+      def remove_ext(name)
+        name.gsub(/.yml/, '')
+      end
+
+      def add_ext(name)
+        name << ".yml" if !(name =~ /.yml/)
+        name
+      end
+
+      def parse_title(name)
+        remove_ext(name).gsub(/\d-/, '').gsub(/-/, '_').humanize
+      end
+
+      def to_permalink(string)
+        string.gsub(/[^\w\s\-\â€”]/,'').gsub(/[^\w]|[\_]/,' ').split.join('-').downcase  
+      end
+
+      def build_filename(title)
+        add_ext(to_permalink("#{count+1}-#{title}"))
+      end
+
+      def path(name)
+        File.join(article_dir, add_ext(name))
+      end
+
+      def parse_data(lines)
+        meta = {}
+        body = []
+
+        lines.each do |line|
+          if line.match(/:meta:([a-z]+):(.+)/)
+            key, val = $1.to_sym, $2
+            val = Date.parse(val) if key.eql?(:date)
+            meta[key] = val
+          else
+            body << line
+          end
+        end
+
+        return body.join(""), meta
+      end
+
+      def article_files(reset = false)
+        @article_files = nil if reset
+
+        @article_files ||= (Dir.glob(File.join('**', "articles", "*")))
+      end
+    
     end
     
     self.article_dir = "articles"
-
-    def self.count(reset = false)
-      article_files(reset).size
-    end
-    
-    def self.all(reset = false, published = true)
-      # remove any falses ..
-      (article_files(reset).map { |filename| Article.open(filename) }).select {|article| article && (article.published == published) }.reverse
-    end
-
-    def self.open(filename)
-      article = false
-      
-      filename << ".yml" if !(filename =~ /.yml/)
-      
-      if File.exist?(filename)
-        data = YAML.load_file(filename)
-        permalink = to_permalink(remove_ext(File.basename(filename)))
-        
-        article = Article.new(data[:content], data, permalink)
-      end
-      
-      article
-    end
-    
-    def self.remove_ext(name)
-      name.gsub(/.yml/, '')
-    end
-    
-    def self.add_ext(name)
-      name << ".yml" if !(name =~ /.yml/)
-      name
-    end
-
-    def self.parse_title(name)
-      remove_ext(name).gsub(/\d-/, '').gsub(/-/, '_').humanize
-    end
-
-    def self.to_permalink(string)
-      string.gsub(/[^\w\s\-\â€”]/,'').gsub(/[^\w]|[\_]/,' ').split.join('-').downcase  
-    end
-    
-    def self.build_filename(title)
-      add_ext(to_permalink("#{count+1}-#{title}"))
-    end
-    
-    def self.path(name)
-      File.join(article_dir, add_ext(name))
-    end
-    
-    def self.parse_data(lines)
-      meta = {}
-      body = []
-      
-      lines.each do |line|
-        if line.match(/:meta:([a-z]+):(.+)/)
-          key, val = $1.to_sym, $2
-          val = Date.parse(val) if key.eql?(:date)
-          meta[key] = val
-        else
-          body << line
-        end
-      end
-      
-      return body.join(""), meta
-    end
-    
-    def self.article_files(reset = false)
-      @article_files = nil if reset
-
-      @article_files ||= (Dir.glob(File.join('**', "articles", "*")))
-    end
 
   end
 
